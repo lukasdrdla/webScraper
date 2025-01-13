a5cff548f3c0fa289cf0e1e4257ec6de6bbe07f9
Drdla <drdla@gmail.com>
2025-01-13 18:08:55 +0100

    configured DI container and registered ScraperService

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using webScraper.Data;
+using webScraper.Services;
+
+var serviceProvider = new ServiceCollection()
+    .AddDbContext<ApplicationDbContext>(options =>
+    {
+        options.UseMySQL("server=localhost;database=webScraper;user=root;");
+    })
+    .AddScoped<ScraperService>()
+    .BuildServiceProvider();
+
+
+var scraperService = serviceProvider.GetRequiredService<ScraperService>();
+
+string url = "https://books.toscrape.com/";
+await scraperService.ScrapeAndSaveBook(url);
+
+Console.WriteLine("Book scraped and saved");
